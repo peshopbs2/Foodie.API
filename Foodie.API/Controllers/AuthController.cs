f1b15e75bcc36ebe732d697a0a0e70012962de37
peshopbs2 <peshopbs2@gmail.com>
2026-01-17 03:28:20 -0800

    add auth controller

---
+++ Foodie.API/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Foodie.Business.Services.Interfaces;
+using Foodie.Models.Dtos.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foodie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequestDto dto)
+        {
+            if(dto == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                LoginResponseDto response = await _authService.AuthenticateAsync(HttpContext, dto);
+                return Ok(response);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
